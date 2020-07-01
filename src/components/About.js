1e11e589df7dae85d2822732f58bf50f57f31cd9
acrophost <anna.piasecka133@gmail.com>
2020-07-01 11:16:27 +0200

    Optimized svg sizes

--- src/components/About.js
+++ src/components/About.js
@@ -6,16 +6,19 @@ const About = () => {
     <div className="about-module">
       <Poly className="background-poly" />
       <h2>About</h2>
-      <p>I'm a 24 year old junior developer from Warsaw, Poland.</p>
-      <p>Always happy to learn new things and challenge myself.</p>
-      <p>
-        Currently unemployed, so if you have any interesting projects or
-        opportunities, please let me know! :)
-      </p>
+      <div className="about-info">
+        <p>I'm a 24 year old junior developer from Warsaw, Poland.</p>
+        <p>Always happy to learn new things and challenge myself.</p>
+        <p>
+          Currently unemployed, so if you have any interesting projects or
+          opportunities, please let me know! :)
+        </p>
+      </div>
+      <div className="about-technology"></div>
       <p>Currently known technologies:</p>
-      <div></div>
-      <p>Learning and willing to learn:</p>
-      <div></div>
+      <div className="about-technology__known"></div>
+      <p>Learning:</p>
+      <div className="about-technology__learning"></div>
     </div>
   );
 };
