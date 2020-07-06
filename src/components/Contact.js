3790f89bf96f6070202151d90b9867b3ae9c4bca
acrophost <anna.piasecka133@gmail.com>
2020-07-06 17:20:34 +0200

    Added contact form base

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,6 +10,21 @@ const Contact = (props) => {
   return (
     <ContactElement ref={props.refs.contact}>
       <h2>Contact me!</h2>
+      <div className="form-group">
+        <label htmlFor="name">Your email</label>
+        <input type="email" className="form-text" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="topic">Topic</label>
+        <input type="text" className="form-text" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="message">Message</label>
+        <textarea className="form-message" rows="5"></textarea>
+      </div>
+      <button type="submit" className="form-button">
+        Send!
+      </button>
     </ContactElement>
   );
 };
