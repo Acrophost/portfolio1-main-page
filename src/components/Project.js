e9884368b45cd262d47d15f1bf5a668c6e873f1d
acrophost <anna.piasecka133@gmail.com>
2020-07-24 17:04:28 +0200

    Added swiping projects on touch

--- src/components/Project.js
+++ src/components/Project.js
@@ -11,6 +11,7 @@ class Project extends React.Component {
     super(props);
     this.state = {
       active: 0,
+      xDown: null,
     };
 
     this.props.refs.projects = React.createRef();
@@ -19,6 +20,8 @@ class Project extends React.Component {
     this.handleCheckLive = this.handleCheckLive.bind(this);
     this.handleLeftClick = this.handleLeftClick.bind(this);
     this.handleRightClick = this.handleRightClick.bind(this);
+    this.handleSwipeStart = this.handleSwipeStart.bind(this);
+    this.handleSwipeMove = this.handleSwipeMove.bind(this);
   }
 
   handleRightClick() {
@@ -47,11 +50,34 @@ class Project extends React.Component {
     navigate(this.state.liveUrl);
   }
 
+  handleSwipeStart(e) {
+    this.setState({ xDown: e.touches[0].clientX });
+  }
+
+  handleSwipeMove(e) {
+    if (!this.state.xDown || this.state.xDown == 0) return;
+    var xDiff = this.state.xDown - e.touches[0].clientX;
+
+    if (Math.abs(xDiff) > 5) {
+      if (xDiff > 0) {
+        this.handleRightClick();
+      } else {
+        this.handleLeftClick();
+      }
+    }
+
+    this.setState({ xDown: null });
+  }
+
   render() {
     const project = this.props.projects[this.state.active];
 
     return (
-      <ProjectElement ref={this.props.refs.projects}>
+      <ProjectElement
+        ref={this.props.refs.projects}
+        onTouchStart={this.handleSwipeStart}
+        onTouchMove={this.handleSwipeMove}
+      >
         <div className="project__triangle-a"></div>
         <h2>Projects</h2>
         <div className="project__swap-info">
