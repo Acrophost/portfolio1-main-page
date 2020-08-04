1b7aa5b79414747f6508a3e31bf92d3819d8425c
acrophost <anna.piasecka133@gmail.com>
2020-08-04 16:32:24 +0200

    Added animation on changing between projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -22,24 +22,77 @@ class Project extends React.Component {
     this.handleRightClick = this.handleRightClick.bind(this);
     this.handleSwipeStart = this.handleSwipeStart.bind(this);
     this.handleSwipeMove = this.handleSwipeMove.bind(this);
+    this.touchStart = this.touchStart.bind(this);
+    this.preventTouch = this.preventTouch.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("touchstart", this.touchStart);
+    window.addEventListener("touchmove", this.preventTouch, { passive: false });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("touchstart", this.touchStart);
+    window.removeEventListener("touchmove", this.preventTouch, {
+      passive: false,
+    });
+  }
+
+  touchStart(e) {
+    this.firstClientX = e.touches[0].clientX;
+    this.firstClientY = e.touches[0].clientY;
+  }
+
+  preventTouch(e) {
+    const minValue = 5; // threshold
+
+    this.clientX = e.touches[0].clientX - this.firstClientX;
+    this.clientY = e.touches[0].clientY - this.firstClientY;
+
+    // Vertical scrolling does not work when you start swiping horizontally.
+    if (Math.abs(this.clientX) > minValue) {
+      e.preventDefault();
+      e.returnValue = false;
+      return false;
+    }
   }
 
   handleRightClick() {
-    this.setState({
-      active:
-        this.state.active + 1 > this.props.projects.length - 1
-          ? 0
-          : this.state.active + 1,
-    });
+    const current = document.getElementsByClassName(
+      "project__images-container"
+    )[1];
+    current.classList.add("fade-left");
+    const next = document.getElementsByClassName("next")[0];
+    next.classList.add("appear-left");
+    setTimeout(() => {
+      current.classList.remove("fade-left");
+      next.classList.remove("appear-left");
+      this.setState({
+        active:
+          this.state.active + 1 > this.props.projects.length - 1
+            ? 0
+            : this.state.active + 1,
+      });
+    }, 1600);
   }
 
   handleLeftClick() {
-    this.setState({
-      active:
-        this.state.active - 1 < 0
-          ? this.props.projects.length - 1
-          : this.state.active - 1,
-    });
+    const current = document.getElementsByClassName(
+      "project__images-container"
+    )[1];
+    current.classList.add("fade-right");
+    const prev = document.getElementsByClassName("prev")[0];
+    prev.classList.add("appear-right");
+    setTimeout(() => {
+      current.classList.remove("fade-right");
+      prev.classList.remove("appear-right");
+      this.setState({
+        active:
+          this.state.active - 1 < 0
+            ? this.props.projects.length - 1
+            : this.state.active - 1,
+      });
+    }, 1600);
   }
 
   handleCheckCode() {
@@ -71,7 +124,17 @@ class Project extends React.Component {
 
   render() {
     const project = this.props.projects[this.state.active];
-
+    const proj_prev = this.props.projects[
+      this.state.active - 1 < 0
+        ? this.props.projects.length - 1
+        : this.state.active - 1
+    ];
+    const proj_next = this.props.projects[
+      this.state.active + 1 == this.props.projects.length
+        ? 0
+        : this.state.active + 1
+    ];
+    console.log(project.description);
     return (
       <ProjectElement
         ref={this.props.refs.projects}
@@ -88,15 +151,39 @@ class Project extends React.Component {
         <button className="project__arrow-left" onClick={this.handleLeftClick}>
           <ArrowSmall className="project__arrow-l" />
         </button>
-        <div className="project__images-container">
-          <img
-            src={project.src}
-            alt={project.alt}
-            className="project__desktop-img"
-          />
-          <div className="project__description-container">
-            <h3>{project.name}</h3>
-            {project.description}
+        <div className="project__container">
+          <div className="project__images-container prev">
+            <img
+              src={proj_prev.src}
+              alt={proj_prev.alt}
+              className="project__desktop-img"
+            />
+            <div className="project__description-container">
+              <h3>{proj_prev.name}</h3>
+              {proj_prev.description}
+            </div>
+          </div>
+          <div className="project__images-container">
+            <img
+              src={project.src}
+              alt={project.alt}
+              className="project__desktop-img"
+            />
+            <div className="project__description-container">
+              <h3>{project.name}</h3>
+              {project.description}
+            </div>
+          </div>
+          <div className="project__images-container next">
+            <img
+              src={proj_next.src}
+              alt={proj_next.alt}
+              className="project__desktop-img"
+            />
+            <div className="project__description-container">
+              <h3>{proj_next.name}</h3>
+              {proj_next.description}
+            </div>
           </div>
         </div>
         <button
